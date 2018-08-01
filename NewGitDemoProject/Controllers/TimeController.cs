c74fc989cef17d368d4d3fbb88f638b6aec70c00
sudhir <sudhir@ICAN-23>
2018-08-01 10:42:22 +0530

    sudhir commit

---
+++ NewGitDemoProject/Controllers/TimeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NewGitDemoProject.Controllers
+{
+    public class TimeController : Controller
+    {
+        // GET: Time
+        public ActionResult Index()
+        {
+            int hour = DateTime.Now.Hour;
+
+            ViewBag.Greeting =
+               hour < 12
+            ? "Good Morning. Time is" + DateTime.Now.ToShortTimeString()
+            : "Good Afternoon. Time is " + DateTime.Now.ToShortTimeString();
+            return View();
+        }
+    }
+}
