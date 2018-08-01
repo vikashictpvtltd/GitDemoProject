61c1cd35c82b02e53bcad84ae3e89c9ed2da801d
Vikash Kumar Tiwari <tiwari.vikash@ictpvtltd.com>
2018-08-01 10:29:55 +0530

    user controller and home changes commited

--- NewGitDemoProject/Controllers/UserController.cs
+++ NewGitDemoProject/Controllers/UserController.cs
@@ -9,9 +9,19 @@ namespace GitDemoProject.Controllers
     public class UserController : Controller
     {
         // GET: User
-        public ActionResult Index()
+        public ActionResult Index(int input1,int input2)
         {
-            ViewBag.Name = "hello user";
+            //get user input
+            ViewBag.Input1 = 0;
+            ViewBag.Input2 = 0;
+            ViewBag.Output = 0;
+            ViewBag.output = input1;
+            if (input1!=0 && input2!=0) {
+                //get user input
+                ViewBag.Input1 = input1;
+                ViewBag.Input2 = input2;
+                ViewBag.Output = input1 + input2;
+            }
             return View(ViewBag);
         }
     }
